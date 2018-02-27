fc6e14d78d20a82c9b50dd433502cf94b4c1781c
Hannah Cushman <hannah.cushman@gmail.com>
2018-02-27 09:27:26 -0600

    Four spaces in JS

--- bga_database/static/js/chart_helper.js
+++ bga_database/static/js/chart_helper.js
@@ -1,60 +1,60 @@
 var ChartHelper = ChartHelper || {};
 
 var ChartHelper = {
-  extract_values: function(data) {
-    var values = Array();
+    extract_values: function(data) {
+        var values = Array();
 
-    data.forEach(function(bin) {
-      values.push(bin.value);
-    });
+        data.forEach(function(bin) {
+            values.push(bin.value);
+        });
 
-    return values;
-  },
-  make_salary_chart: function(data, entity_type) {
-    var values = ChartHelper.extract_values(data);
+        return values;
+    },
+    make_salary_chart: function(data, entity_type) {
+        var values = ChartHelper.extract_values(data);
 
-    var tooltip_format = function(point) {
-      var edges = data[this.x];
-      return this.y + ' ' + entity_type + 's earn between $' + edges.lower_edge + ' and $' + edges.upper_edge;
-    };
+        var tooltip_format = function(point) {
+            var edges = data[this.x];
+            return this.y + ' ' + entity_type + 's earn between $' + edges.lower_edge + ' and $' + edges.upper_edge;
+        };
 
-    var axis_format = function() {
-      var edges = data[this.value];
-      return '$' + edges.lower_edge + ' – $' + edges.upper_edge;
-    };
+        var axis_format = function() {
+            var edges = data[this.value];
+            return '$' + edges.lower_edge + ' – $' + edges.upper_edge;
+        };
 
-    Highcharts.chart('distribution-chart', {
-      title: {
-        text: '', // Done in template
-      },
-      xAxis: {
-        labels: {
-          enabled: true,
-          formatter: axis_format,
-        },
-        title: {
-          text: 'Salary range',
-        },
-      },
-      yAxis: {
-        title: {
-          text: 'Number of ' + entity_type + 's',
-        },
-      },
-      series: [{
-        name: entity_type + 's',
-        type: 'column',
-        data: values,
-        id: 'salaries',
-        tooltip: {
-          headerFormat: '', // Remove header
-          pointFormatter: tooltip_format,
-        },
-        color: '#6c757c',
-      }],
-      legend: {
-        enabled: false,
-      }
-    });
-  },
+        Highcharts.chart('distribution-chart', {
+            title: {
+                text: '', // Done in template
+            },
+            xAxis: {
+                labels: {
+                    enabled: true,
+                    formatter: axis_format,
+                },
+                title: {
+                    text: 'Salary range',
+                },
+            },
+            yAxis: {
+                title: {
+                    text: 'Number of ' + entity_type + 's',
+                },
+            },
+            series: [{
+                name: entity_type + 's',
+                type: 'column',
+                data: values,
+                id: 'salaries',
+                tooltip: {
+                    headerFormat: '', // Remove header
+                    pointFormatter: tooltip_format,
+                },
+                color: '#6c757c',
+            }],
+            legend: {
+                enabled: false,
+            }
+        });
+    },
 };
