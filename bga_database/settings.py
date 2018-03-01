b25cf1786f8962db9bb62ff18c30ac6145580b5f
Hannah Cushman <hannah.cushman@gmail.com>
2018-03-01 14:05:55 -0600

    Update settings

--- bga_database/settings.py
+++ bga_database/settings.py
@@ -14,7 +14,11 @@ import os
 from .local_settings import *  # noqa
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.datamade.us',
+]
 
 # Application definition
 
