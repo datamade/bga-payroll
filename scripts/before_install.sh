bba7979aa12b779752953d6bb97f323f11a5b1ae
Hannah Cushman <hannah.cushman@gmail.com>
2018-03-01 15:07:58 -0600

    Remove the old folder

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+rm -Rf /home/datamade/bga-payroll
 mkdir -p /home/datamade/bga-payroll
 
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade .
