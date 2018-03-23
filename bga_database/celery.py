fe09bd6585739bd61c59ebe99b91b4a8f0d5a811
Hannah Cushman <hannah.cushman@gmail.com>
2018-03-23 16:08:25 -0500

    Wire up Celery/Redis for delayed tasks

---
+++ bga_database/celery.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+from celery import Celery
+from django.conf import settings
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bga_database.settings')
+
+app = Celery('bga_database')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
