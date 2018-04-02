1f5f8aaf39693b16b29b9f264cdd5c5f673280c0
Hannah Cushman <hannah.cushman@gmail.com>
2018-04-02 15:14:19 -0500

    Strike unnecessary employer query, add salary query

--- data_import/utils/import_utility.py
+++ data_import/utils/import_utility.py
@@ -16,31 +16,10 @@ class ImportUtility(object):
         self.raw_person_table = make_table_name('person')
 
     def import_new(self):
-        self.select_raw_employer()
         self.select_raw_position()
         self.insert_employer()
         self.insert_position()
-
-    def select_raw_employer(self):
-        query = '''
-            SELECT *
-            INTO {raw_employer}
-            FROM (
-              SELECT
-                DISTINCT employer AS employer_name,
-                NULL AS parent_name
-              FROM {raw_payroll}
-              UNION
-              SELECT DISTINCT ON (employer, department)
-                department AS employer_name,
-                employer AS parent_name
-              FROM {raw_payroll}
-            ) AS employers
-        '''.format(raw_employer=self.raw_employer_table,
-                   raw_payroll=self.raw_payroll_table)
-
-        with connection.cursor() as cursor:
-            cursor.execute(query)
+        self.select_raw_salary()
 
     def select_raw_position(self):
         query = '''
@@ -59,34 +38,21 @@ class ImportUtility(object):
             cursor.execute(query)
 
     def insert_employer(self):
-        '''
-        - Select employers with no parent, and insert them into
-          payroll_employer.
-        - Join employers with a parent with payroll_employer to
-          retrieve the appropriate parent ID, and insert them into
-          payroll employer.
-
-        n.b. In both tables, parent is null for top-level employers.
-        '''
         insert_parents = '''
             INSERT INTO payroll_employer (name)
-              SELECT
-                employer_name
-              FROM {}
-              WHERE parent_name IS NULL
-        '''.format(self.raw_employer_table)
+              SELECT DISTINCT employer FROM {}
+        '''.format(self.raw_payroll_table)
 
         insert_children = '''
             INSERT INTO payroll_employer (name, parent_id)
-              SELECT
-                child.employer_name,
-                parent.id
+              SELECT DISTINCT ON (employer, department)
+                department AS employer_name,
+                parent.id AS parent_id
               FROM {} AS child
               JOIN payroll_employer AS parent
-              ON child.parent_name = parent.name
-              WHERE parent.parent_id IS NULL
-              AND child.employer_name IS NOT NULL
-        '''.format(self.raw_employer_table)
+              ON child.employer = parent.name
+              WHERE department IS NOT NULL
+        '''.format(self.raw_payroll_table)
 
         with connection.cursor() as cursor:
             cursor.execute(insert_parents)
@@ -106,19 +72,12 @@ class ImportUtility(object):
                 child.name AS employer_name,
                 parent.name AS parent_name
               FROM payroll_employer AS child
-              JOIN payroll_employer AS parent
+              LEFT JOIN payroll_employer AS parent
               ON child.parent_id = parent.id
-              UNION
-              SELECT
-                id,
-                name AS employer_name,
-                null AS parent_name
-              FROM payroll_employer
-              WHERE parent_id IS NULL
             )
             SELECT DISTINCT ON (employer, department, title)
               employer_id,
-              title
+              COALESCE(title, 'EMPLOYEE')
             FROM {} AS raw
             JOIN employer_ids AS emp
             ON COALESCE(raw.department, raw.employer) = emp.employer_name
@@ -132,8 +91,42 @@ class ImportUtility(object):
         with connection.cursor() as cursor:
             cursor.execute(insert)
 
-    def insert_salary(self):
-        pass
+    def select_raw_salary(self):
+        query = '''
+            WITH named_positions AS (
+              SELECT
+                pos.id AS position_id,
+                pos.title AS position_title,
+                emp.id AS employer_id,
+                emp.name AS employer_name,
+                parent.name AS parent_name
+              FROM payroll_position AS pos
+              JOIN payroll_employer AS emp
+              ON pos.employer_id = emp.id
+              LEFT JOIN payroll_employer AS parent
+              ON emp.parent_id = parent.id
+            )
+            SELECT
+              record_id,
+              position_id,
+              salary,
+              date_started,
+              data_year
+            INTO {raw_salary}
+            FROM {raw_payroll} AS raw
+            JOIN named_positions AS pos
+            ON COALESCE(raw.title, 'EMPLOYEE') = pos.position_title
+            AND COALESCE(raw.department, raw.employer) = pos.employer_name
+            AND CASE WHEN raw.department IS NOT NULL THEN raw.employer
+                ELSE 'No parent'
+                END = CASE WHEN raw.department IS NOT NULL THEN pos.parent_name
+                ELSE 'No parent'
+                END
+        '''.format(raw_salary=self.raw_salary_table,
+                   raw_payroll=self.raw_payroll_table)
+
+        with connection.cursor() as cursor:
+            cursor.execute(query)
 
     def insert_person(self):
         pass
