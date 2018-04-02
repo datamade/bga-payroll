ea676f545e343b2d78cdbb2268575aa3cda59d85
Hannah Cushman <hannah.cushman@gmail.com>
2018-04-02 16:15:34 -0500

    Create unique index on employer & position

--- data_import/utils/import_utility.py
+++ data_import/utils/import_utility.py
@@ -16,13 +16,46 @@ class ImportUtility(object):
         self.raw_person_table = make_table_name('person')
 
     def import_new(self):
-        self.select_raw_position()
         self.insert_employer()
+        self.select_raw_position()
         self.insert_position()
         self.select_raw_salary()
 
+    def insert_employer(self):
+        '''
+        Insert parents that do not already exist, then insert
+        departments, no-op'ing on duplicates via the unique index
+        on payroll_employer name / parent ID. The uniqueness of
+        everything else is predicated on the uniqueness of the
+        parents. TO-DO: Explain this in better English!
+        '''
+        insert_parents = '''
+            INSERT INTO payroll_employer (name)
+              SELECT DISTINCT employer
+              FROM {} AS raw
+              LEFT JOIN payroll_employer AS existing
+              ON raw.employer = existing.name
+              WHERE existing.name IS NULL
+        '''.format(self.raw_payroll_table)
+
+        insert_children = '''
+            INSERT INTO payroll_employer (name, parent_id)
+              SELECT DISTINCT ON (employer, department)
+                department AS employer_name,
+                parent.id AS parent_id
+              FROM {} AS child
+              JOIN payroll_employer AS parent
+              ON child.employer = parent.name
+              WHERE department IS NOT NULL
+            ON CONFLICT DO NOTHING
+        '''.format(self.raw_payroll_table)
+
+        with connection.cursor() as cursor:
+            cursor.execute(insert_parents)
+            cursor.execute(insert_children)
+
     def select_raw_position(self):
-        query = '''
+        select = '''
             SELECT DISTINCT ON (employer, department, title)
               COALESCE(department, employer) AS employer_name,
               CASE WHEN department IS NULL THEN NULL
@@ -35,64 +68,39 @@ class ImportUtility(object):
                    raw_payroll=self.raw_payroll_table)
 
         with connection.cursor() as cursor:
-            cursor.execute(query)
-
-    def insert_employer(self):
-        insert_parents = '''
-            INSERT INTO payroll_employer (name)
-              SELECT DISTINCT employer FROM {}
-        '''.format(self.raw_payroll_table)
-
-        insert_children = '''
-            INSERT INTO payroll_employer (name, parent_id)
-              SELECT DISTINCT ON (employer, department)
-                department AS employer_name,
-                parent.id AS parent_id
-              FROM {} AS child
-              JOIN payroll_employer AS parent
-              ON child.employer = parent.name
-              WHERE department IS NOT NULL
-        '''.format(self.raw_payroll_table)
-
-        with connection.cursor() as cursor:
-            cursor.execute(insert_parents)
-            cursor.execute(insert_children)
+            cursor.execute(select)
 
     def insert_position(self):
-        '''
-        Get names, IDs, and (if applicable) parents from the
-        employer table, and join to the raw table to select a list
-        of positions, e.g., distinct employer / title combinations.
-        '''
         insert = '''
             INSERT INTO payroll_position (employer_id, title)
-            WITH employer_ids AS (
-              SELECT
-                child.id AS employer_id,
-                child.name AS employer_name,
-                parent.name AS parent_name
-              FROM payroll_employer AS child
-              LEFT JOIN payroll_employer AS parent
-              ON child.parent_id = parent.id
-            )
-            SELECT DISTINCT ON (employer, department, title)
-              employer_id,
-              COALESCE(title, 'EMPLOYEE')
-            FROM {} AS raw
-            JOIN employer_ids AS emp
-            ON COALESCE(raw.department, raw.employer) = emp.employer_name
-            AND CASE WHEN raw.department IS NOT NULL THEN raw.employer
-                ELSE 'No parent'
-                END = CASE WHEN raw.department IS NOT NULL THEN emp.parent_name
-                ELSE 'No parent'
-                END
+              WITH employer_ids AS (
+                SELECT
+                  child.id AS employer_id,
+                  child.name AS employer_name,
+                  parent.name AS parent_name
+                FROM payroll_employer AS child
+                LEFT JOIN payroll_employer AS parent
+                ON child.parent_id = parent.id
+              )
+              SELECT DISTINCT ON (employer, department, title)
+                employer_id,
+                COALESCE(title, 'EMPLOYEE')
+              FROM {} AS raw
+              JOIN employer_ids AS emp
+              ON COALESCE(raw.department, raw.employer) = emp.employer_name
+              AND CASE WHEN raw.department IS NOT NULL THEN raw.employer
+                  ELSE 'No parent'
+                  END = CASE WHEN raw.department IS NOT NULL THEN emp.parent_name
+                  ELSE 'No parent'
+                  END
+            ON CONFLICT DO NOTHING
         '''.format(self.raw_payroll_table)
 
         with connection.cursor() as cursor:
             cursor.execute(insert)
 
     def select_raw_salary(self):
-        query = '''
+        select = '''
             WITH named_positions AS (
               SELECT
                 pos.id AS position_id,
@@ -126,7 +134,10 @@ class ImportUtility(object):
                    raw_payroll=self.raw_payroll_table)
 
         with connection.cursor() as cursor:
-            cursor.execute(query)
+            cursor.execute(select)
+
+    def select_raw_person(self):
+        pass
 
     def insert_person(self):
         pass
--- tests/data_import/test_utils.py
+++ tests/data_import/test_utils.py
@@ -1,4 +1,5 @@
 from django.core.files.uploadedfile import UploadedFile
+from django.db import connection
 import pytest
 
 from data_import.exceptions import OperationNotPermittedOnInstance
@@ -35,4 +36,19 @@ def test_import_utility(standardized_file,
     imp = ImportUtility(s_file.id)
     imp.import_new()
 
-    # test some stuff
+    with connection.cursor() as cursor:
+        cursor.execute('''
+            SELECT
+              (SELECT COUNT(*) FROM {raw_payroll}) AS raw_count,
+              (SELECT COUNT(*) FROM {raw_salary}) AS generated_count
+        '''.format(raw_payroll=imp.raw_payroll_table,
+                   raw_salary=imp.raw_salary_table))
+
+        result, = cursor
+        raw_count, generated_count = result
+
+        # Test we make a salary item for each record.
+
+        assert raw_count == generated_count
+
+    # test some other stuff
