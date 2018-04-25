0cdbf06bac15ff3d5acf3779cb89389fe60fbe78
Hannah Cushman <hannah.cushman@gmail.com>
2018-04-25 11:16:25 -0500

    Extend docstrings in celery tasks module

--- data_import/tasks.py
+++ data_import/tasks.py
@@ -10,8 +10,26 @@ from data_import.utils import CsvMeta, ImportUtility
 class DataImportTask(Task):
     def setup(self, s_file_id):
         '''
-        Extending init breaks test discovery in Django. Instead,
-        define a setup method that sets common attributes for tasks.
+        The __init__ method of the Task class is only called once
+        per process, which means it cannot be used to generate
+        dynamic attributes (such as s_file and import_utility,
+        which depend on the StandardizedFile at hand).
+
+        When a task method inherits from a base Task class (like
+        this one), its code is injected into the run method of that
+        class. However, we cannot define a run method on this base
+        class and extend common functionality via super() in the
+        task methods, because the task methods *become* the run
+        method of this class, e.g., the call to super() will execute
+        the run method of this class's parent, celery's Task class,
+        which raises a NotImplementedError.
+
+        Moreover, extending the run method defeats the purpose of
+        __init__-like behavior, e.g., auto-generated context, because
+        super() would have to be called by each task. Instead, we define
+        a setup method to establish common context, and run it before
+        each task via celery's task_prerun signal. (See the decorated
+        init_task method, below.)
         '''
         from data_import.models import StandardizedFile
 
@@ -24,25 +42,35 @@ class DataImportTask(Task):
 
 
 @task_prerun.connect()
-def task_prerun(sender=DataImportTask, *args, **kwargs):
+def init_task(*args, **kwargs):
     '''
-    In lieu of an extended init on the custom Task object,
-    hook into the task_prerun signal. This is fired before
-    all tasks are run.
-    '''
-    s_file_id = kwargs['kwargs']['s_file_id']
+    Celery issues signals for a pre-defined list of task, worker,
+    and application events:
+    http://docs.celeryproject.org/en/latest/userguide/signals.html#signals
 
-    if s_file_id:
+    This method hooks into the task_prerun signal, which is fired
+    before each task is run. This signal provides access to the task
+    class being executed via the sender kwarg, as well as args and kwargs
+    from the task call. This provides a convenient hook for running
+    our setup method automatically, achieving __init__-like behavior,
+    in lieu of access to the actual __init__ method of our task class.
+    '''
+    sender = kwargs['sender']
+
+    if isinstance(sender, DataImportTask):
+        # Each task method must receive an s_file_id keyword-only
+        # argument, hence we can assume its existence.
+        s_file_id = kwargs['kwargs']['s_file_id']
         sender.setup(s_file_id)
 
 
 @shared_task(bind=True, base=DataImportTask)
 def copy_to_database(self, *, s_file_id):
     '''
-    Define a task method, and bind it to the base task.
-    The setup method of the base task will be fired before
-    the code in this task method, e.g, self.s_file and
-    self.import_utility have been defined.
+    Define a task method, and bind it to the base task. The setup
+    method of the base task will be fired by the task_prerun signal
+    handler, init_task, before the code in this task method is run,
+    e.g, self.s_file and self.import_utility are available.
     '''
     table_name = self.s_file.raw_table_name
 
