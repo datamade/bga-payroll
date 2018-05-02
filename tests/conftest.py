b47ff48eb45106208e6182873470a16d7c973010
Hannah Cushman <hannah.cushman@gmail.com>
2018-05-02 16:10:09 -0500

    Add test_queues

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from data_import.models import Upload
+from payroll.models import Employer
 
 
 @pytest.fixture
@@ -14,3 +15,19 @@ def upload():
             return Upload.objects.create(**data)
 
     return UploadFactory()
+
+
+@pytest.fixture
+@pytest.mark.django_db(transaction=True)
+def employer(upload, transactional_db):
+    class EmployerFactory():
+        def build(self, **kwargs):
+            data = {
+                'name': 'Half Acre',
+                'vintage': upload.build(),
+            }
+            data.update(kwargs)
+
+            return Employer.objects.create(**data)
+
+    return EmployerFactory()
