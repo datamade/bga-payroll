c1fbe7ebca7e632ae4904ff15ad3de93e25c5e89
Hannah Cushman <hannah.cushman@gmail.com>
2018-05-14 15:02:50 -0500

    Repair recipe, allow for larger uploads

--- data_samples.mk
+++ data_samples.mk
@@ -20,7 +20,7 @@ data/output/2016-sample-b.csv : 2016-formatted.csv
 
 2016-formatted.csv : raw/2016_payroll.csv
 	csvcut -c 12,12,6,4,3,5,7,8,11 -e ISO-8859-1 $< | tail +2 | \
-	(echo $(SAMPLE_HEADER); python processors/convert_date.py) > $@
+	(echo $(SAMPLE_HEADER); python data/processors/convert_date.py) > $@
 
 2017-formatted.csv : raw/2017_payroll.csv
 	csvcut -c 2,2,3,4,5,6,7,8,9 -e WINDOWS-1250 $< | \
