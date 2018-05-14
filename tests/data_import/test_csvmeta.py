622e4aca90bf15fee5cf20fe751e1f1fac2f93ec
Hannah Cushman <hannah.cushman@gmail.com>
2018-05-14 16:06:33 -0500

    Scale file encoding detection for large files

--- tests/data_import/test_csvmeta.py
+++ tests/data_import/test_csvmeta.py
@@ -8,10 +8,13 @@ from data_import import utils
 def test_alter_uploadedfile_raises_exception(mocker):
     mock_uploadedfile = mocker.MagicMock(spec=UploadedFile)
 
+    # Return a generator from file.chunks() so init doesn't fail.
+    mock_uploadedfile.chunks.return_value = iter(['foo'])
+
     # Patch _field_names setter to avoid StopIteration as result of
     # passing in mock (e.g., empty) file
-
     mocker.patch.object(utils.CsvMeta, '_field_names')
+    mocker.patch.object(utils.CsvMeta, '_file_encoding')
 
     meta = utils.CsvMeta(mock_uploadedfile)
 
