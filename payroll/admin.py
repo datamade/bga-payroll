2c85f75062dc5d05f0bb9aa0856d39a4cdf339a9
Hannah Cushman <hannah.cushman@gmail.com>
2018-06-25 16:30:39 -0500

    Wire up admin access for Employer

---
+++ payroll/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from payroll.models import Employer
+
+
+class AdminEmployer(admin.ModelAdmin):
+    ordering = ('name',)
+    search_fields = ('name',)
+    raw_id_fields = ('parent',)
+    readonly_fields = ('slug', 'parent', 'vintage',)
+
+admin.site.register(Employer, AdminEmployer)
