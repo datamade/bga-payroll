a5915b4ecc4df516fc5dfe1e300c11d6e2e3c6b2
Hannah Cushman <hannah.cushman@gmail.com>
2018-07-05 15:02:24 -0500

    Enable EmployerUniverse in admin, remove school district as universe

--- payroll/admin.py
+++ payroll/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from payroll.models import Employer
+from payroll.models import Employer, EmployerUniverse
 
 
 class AdminEmployer(admin.ModelAdmin):
@@ -9,4 +9,9 @@ class AdminEmployer(admin.ModelAdmin):
     readonly_fields = ('slug', 'parent', 'vintage',)
 
 
+class AdminEmployerUniverse(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Employer, AdminEmployer)
+admin.site.register(EmployerUniverse, AdminEmployerUniverse)
