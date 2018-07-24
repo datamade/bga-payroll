eb5b7d06037ea0a45e1f57c8beffcbef15b95f9f
Hannah Cushman <hannah.cushman@gmail.com>
2018-07-24 16:44:04 -0500

    Remove duplicate bin code

--- payroll/views.py
+++ payroll/views.py
@@ -1,5 +1,4 @@
 import json
-import math
 
 from django.db import connection
 from django.db.models import Q, FloatField
@@ -9,13 +8,11 @@ from django.urls import reverse
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-import numpy as np
 from postgres_stats.aggregates import Percentile
 
 from payroll.charts import ChartHelperMixin
 from payroll.models import Employer, Job, Person, Salary, Unit, Department
 from payroll.search import PayrollSearchMixin, FacetingMixin
-from payroll.utils import format_ballpark_number
 
 
 class IndexView(TemplateView, ChartHelperMixin):
@@ -131,35 +128,6 @@ class EmployerView(DetailView, ChartHelperMixin):
             employee_salaries = [row[0] for row in cursor]
 
         return employee_salaries
-
-    def bin_salary_data(self, data):
-        float_data = np.asarray(data, dtype='float')
-
-        # Size of the bins
-        multiplier = 25000
-
-        # This is to make the appropriate number of bins
-        max_value = np.amax(float_data)
-        bin_num = math.ceil(max_value / multiplier)  # rounding up to capture max value
-        bin_edges = np.array([], dtype='float')
-
-        for i in range(bin_num + 1):  # adding 1 to get appropriate number of bins
-            bin_edges = np.append(bin_edges, i * multiplier)
-
-        values, edges = np.histogram(float_data, bins=bin_edges)
-
-        salary_json = []
-
-        for i, value in enumerate(values):
-            lower, upper = int(edges[i]), int(edges[i + 1])
-
-            salary_json.append({
-                'value': int(value),  # number of salaries in given bin
-                'lower_edge': format_ballpark_number(lower),
-                'upper_edge': format_ballpark_number(upper),
-            })
-
-        return salary_json
 
 
 class UnitView(EmployerView):
