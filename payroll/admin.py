306a8473e40751bb10f0b19231856bc0a6f47994
Hannah Cushman <hannah.cushman@gmail.com>
2018-09-12 14:01:06 -0500

    Reindex employers on edit

--- payroll/admin.py
+++ payroll/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.core.management import call_command
+
 from payroll.models import Employer, EmployerUniverse, EmployerTaxonomy
 
 
@@ -7,6 +9,10 @@ class AdminEmployer(admin.ModelAdmin):
     search_fields = ('name',)
     raw_id_fields = ('parent',)
     readonly_fields = ('slug', 'parent', 'vintage',)
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        call_command('build_solr_index', employer=obj.id)
 
 
 class AdminEmployerUniverse(admin.ModelAdmin):
