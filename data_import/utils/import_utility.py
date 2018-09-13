f61df5f8c188bbcbcf2b718d36982cbc893d4431
Hannah Cushman <hannah.cushman@gmail.com>
2018-09-13 14:36:34 -0500

    Add unique index to prevent duplicate responding agency / unit relationships

--- data_import/utils/import_utility.py
+++ data_import/utils/import_utility.py
@@ -308,7 +308,10 @@ class ImportUtility(TableNamesMixin):
             ON TRIM(LOWER(raw.employer)) = TRIM(LOWER(emp.name))
             JOIN data_import_respondingagency AS agency
             ON TRIM(LOWER(raw.responding_agency)) = TRIM(LOWER(agency.name))
+            LEFT JOIN payroll_unitrespondingagency AS ura
+            ON emp.id = ura.unit_id
             WHERE emp.parent_id IS NULL
+            ON CONFLICT DO NOTHING
         '''.format(reporting_year=self.reporting_year,
                    intermediate_payroll=self.intermediate_payroll_table)
 
