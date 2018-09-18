86806dcf477073be5e41e59a640fb1186a0db3aa
Hannah Cushman <hannah.cushman@gmail.com>
2018-09-18 09:28:18 -0500

    Account for different date formats in data making

--- data/processors/validate_dates.py
+++ data/processors/validate_dates.py
@@ -10,9 +10,19 @@ writer.writerow(next(reader))
 
 for row in reader:
     if row[-5]:
-        try:
-            datetime.datetime.strptime(row[-5], '%m/%d/%y')
-        except ValueError:  # invalid date
+        date_validated = False
+
+        for date_format in ('%m/%d/%y', '%m/%d/%Y', '%Y-%m-%d'):
+            try:
+                datetime.datetime.strptime(row[-5], date_format)
+            except ValueError:  # invalid date
+                continue
+            else:
+                date_validated = True
+                break
+
+        if not date_validated:
+            sys.stderr.write('{}\n'.format(row[-5]))
             row[-5] = ''
 
-    writer.writerow(row)
+        writer.writerow(row)
