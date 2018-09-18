c16a4fedcac9f1b70495896f8fd8a2a9bb4ee82c
Hannah Cushman <hannah.cushman@gmail.com>
2018-09-18 08:59:31 -0500

    Refactor ballpark to add sig fig to single-digit estimates

--- payroll/utils.py
+++ payroll/utils.py
@@ -24,36 +24,35 @@ def format_salary(i):
 def format_ballpark_number(i):
     '''
     Given an integer, i, return a shortened form of the number, i.e.,
-    1,000 = 1k, 2,000,000 = 2 million, etc.
+    10,000 = 10k, 2,000,000 = 2.0 million, etc. Return a string.
     '''
-    try:
-        i = int(i)
+    buckets = [
+        (1000, 'k'),
+        (1000000, ' million'),
+        (1000000000, ' billion'),
+        (1000000000000, ' trillion'),
+    ]
 
-    except TypeError:
-        raise TypeError('i must be coerceable to an integer')
+    def n_sig_figs(floating_point):
+        integer = int(floating_point)
+        return len(str(integer))
 
-    def truncate(i):
-        return str(i)[:-3]
+    for b, suffix in buckets:
+        quotient = float(i / b)
 
-    if len(truncate(i)) < 1:
-        return str(i)
-    else:
-        truncated_i = truncate(i)
+        if quotient >= 1000:
+            continue
 
-    truncate_count = 1
+        elif quotient < 1:
+            return str(i)
 
-    while len(truncated_i) > 3:
-        truncated_i = truncate(truncated_i)
-        truncate_count += 1
+        else:
+            if n_sig_figs(quotient) > 1:
+                number = int(quotient)
+            else:
+                number = round(quotient, 1)
 
-    suffix_map = {
-        1: 'k',
-        2: ' million',
-        3: ' billion',
-        4: ' trillion',
-    }
-
-    return truncated_i + suffix_map[truncate_count]
+            return '{0}{1}'.format(number, suffix)
 
 
 def format_percentile(i):
