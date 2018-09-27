d060bb0c2500e38180b491d2ea1b6aecaede3ed1
Hannah Cushman <hannah.cushman@gmail.com>
2018-09-27 15:07:11 -0500

    Add logging in useful places

--- data/amended_files.mk
+++ data/amended_files.mk
@@ -6,13 +6,17 @@ VPATH=data
 	%-amendment-no-salary-omitted.csv group-%.csv
 
 %-amendment.csv : %-amendment-no-salary-omitted.csv
+	# Rename fields.
 	(echo employer,last_name,first_name,title,department,base_salary,extra_pay,date_started,id,responding_agency,data_year,salary; \
 	tail -n +2 $<) > $@
 
 %-amendment-no-salary-omitted.csv : %-amendment-salary-summed.csv
-	csvgrep -c total_pay -r '^$$' -i $< > $@
+	# Remove records where no salary was reported.
+	csvgrep -c total_pay -r '^$$' -i $< > $@;
+	echo "Removed $$(csvgrep -c total_pay -r '^$$' $< | wc -l | xargs) records without salary"
 
 %-amendment-salary-summed.csv : %-amendment-with-valid-start-dates.csv
+	# Sum base pay and extra salary.
 	cat $< | python data/processors/sum_salary.py > $@
 
 %-amendment-with-valid-start-dates.csv : %-amendment-with-data-year.csv
@@ -24,10 +28,11 @@ VPATH=data
 	perl -pe 's/$$/,2017/' $< > $@
 
 .SECONDEXPANSION :
-%-amendment-with-agencies.csv : $$(wildcard raw/$$*-amendment.csv $$*.csv) raw/foia-source-lookup.csv
+%-amendment-with-agencies.csv : $$(wildcard data/raw/$$*-amendment.csv $$*.csv) raw/foia-source-lookup.csv
 	# Join standard data with agency lookup.
 	csvjoin -c id,ID -e IBM852 $^ > $@
 
 group-%.csv : raw/amendments-%
-	# Combine a group of amended files.
-	csvstack $</* > $@
+	# Combine a group of amended files. Remove lines all-null lines, i.e.,
+	# those starting with a comma.
+	csvstack $</* | grep -E '^,' -v > $@
