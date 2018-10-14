fe6f0c44be432a60e79f8e4af3c285feceaaef5c
Hannah Cushman <hannah.cushman@gmail.com>
2018-10-14 05:29:18 -0500

    Make action browser readonly

--- payroll/admin.py
+++ payroll/admin.py
@@ -24,7 +24,26 @@ class AdminEmployerTaxonomy(admin.ModelAdmin):
     pass
 
 
+class LogEntryAdmin(admin.ModelAdmin):
+    readonly_fields = ('content_type',
+        'user',
+        'action_time',
+        'object_id',
+        'object_repr',
+        'action_flag',
+        'change_message'
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_actions(self, request):
+        actions = super(LogEntryAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+
 admin.site.register(Employer, AdminEmployer)
 admin.site.register(EmployerUniverse, AdminEmployerUniverse)
 admin.site.register(EmployerTaxonomy, AdminEmployerTaxonomy)
-admin.site.register(LogEntry)
+admin.site.register(LogEntry, LogEntryAdmin)
