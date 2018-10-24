4c098694da587c53774cc540aea46abe59802965
Hannah Cushman <hannah.cushman@gmail.com>
2018-10-24 16:09:09 -0500

    Use logged in client for data import tests
    God bless pytest-django

--- tests/data_import/test_upload.py
+++ tests/data_import/test_upload.py
@@ -19,7 +19,7 @@ def test_valid_source_file_upload():
 @pytest.mark.standardized_data
 def test_missing_fields_raises_exception(standardized_data_upload_blob,
                                          mocker,
-                                         client):
+                                         admin_client):
 
     bad_fields = ['not', 'the', 'right', 'fields']
     mock_get_fields = mocker.patch.object(CsvMeta, '_field_names')
@@ -27,9 +27,9 @@ def test_missing_fields_raises_exception(standardized_data_upload_blob,
 
     mock_file = standardized_data_upload_blob['standardized_file']
 
-    rv = client.post(reverse('upload-standardized-file'),
-                     data=standardized_data_upload_blob,
-                     files={'standardized_file': mock_file})
+    rv = admin_client.post(reverse('upload-standardized-file'),
+                           data=standardized_data_upload_blob,
+                           files={'standardized_file': mock_file})
 
     assert rv.status_code != 302
     assert 'Standardized file missing fields:' in rv.content.decode('utf-8')
@@ -38,16 +38,16 @@ def test_missing_fields_raises_exception(standardized_data_upload_blob,
 @pytest.mark.django_db
 @pytest.mark.standardized_data
 def test_non_csv_raises_exception(standardized_data_upload_blob,
-                                  client):
+                                  admin_client):
 
     mock_file = standardized_data_upload_blob['standardized_file']
     mock_file.name = 'not_a_csv.xlsx'
 
     standardized_data_upload_blob['standardized_file'] = mock_file
 
-    rv = client.post(reverse('upload-standardized-file'),
-                     data=standardized_data_upload_blob,
-                     files={'standardized_file': mock_file})
+    rv = admin_client.post(reverse('upload-standardized-file'),
+                           data=standardized_data_upload_blob,
+                           files={'standardized_file': mock_file})
 
     assert rv.status_code != 302
     assert 'Please upload a CSV' in rv.content.decode('utf-8')
@@ -56,16 +56,16 @@ def test_non_csv_raises_exception(standardized_data_upload_blob,
 @pytest.mark.django_db
 @pytest.mark.standardized_data
 def test_future_date_raises_exception(standardized_data_upload_blob,
-                                      client):
+                                      admin_client):
 
     ten_years_from_now = datetime.datetime.today().year + 10
     standardized_data_upload_blob['reporting_year'] = ten_years_from_now
 
     mock_file = standardized_data_upload_blob['standardized_file']
 
-    rv = client.post(reverse('upload-standardized-file'),
-                     data=standardized_data_upload_blob,
-                     files={'standardized_file': mock_file})
+    rv = admin_client.post(reverse('upload-standardized-file'),
+                           data=standardized_data_upload_blob,
+                           files={'standardized_file': mock_file})
 
     assert rv.status_code != 302
     assert 'Reporting year cannot exceed the current year' in rv.content.decode('utf-8')
@@ -75,7 +75,7 @@ def test_future_date_raises_exception(standardized_data_upload_blob,
 @pytest.mark.standardized_data
 def test_valid_standardized_data_upload(standardized_data_upload_blob,
                                         real_file,
-                                        client,
+                                        admin_client,
                                         mocker):
 
     # Mock our delayed tasks (which we'll test over in test_tasks)
@@ -83,9 +83,9 @@ def test_valid_standardized_data_upload(standardized_data_upload_blob,
 
     standardized_data_upload_blob['standardized_file'] = real_file
 
-    rv = client.post(reverse('upload-standardized-file'),
-                     data=standardized_data_upload_blob,
-                     files={'standardized_file': real_file})
+    rv = admin_client.post(reverse('upload-standardized-file'),
+                           data=standardized_data_upload_blob,
+                           files={'standardized_file': real_file})
 
     # Assert the page redirects, e.g., upload was successful
     assert rv.status_code == 302
