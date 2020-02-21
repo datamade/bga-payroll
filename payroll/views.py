ebb73dbed43fbafb6a698d0b39b19cc5c47ab51a
Fatima Gomez <fatima.gomez@datamade.us>
2020-02-21 13:59:40 -0600

    fix linting errors

--- payroll/views.py
+++ payroll/views.py
@@ -159,13 +159,13 @@ class EmployerView(DetailView, ChartHelperMixin):
         )
         median_total_pay = Percentile(
             (
-                Coalesce("positions__jobs__salaries__amount", 0) + 
+                Coalesce("positions__jobs__salaries__amount", 0) +
                 Coalesce("positions__jobs__salaries__extra_pay", 0)
             ), 0.5, output_field=FloatField()
         )
 
         results = entity_qs.all().aggregate(
-            median_base_pay=median_base_pay, 
+            median_base_pay=median_base_pay,
             median_extra_pay=median_extra_pay,
             median_total_pay=median_total_pay
         )
