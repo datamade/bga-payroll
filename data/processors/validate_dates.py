d37624d24f13b1f23570ea4805c7119ad1c3298b
hancush <hannah.cushman@gmail.com>
2020-02-25 09:31:10 -0600

    Stash 2018 data

--- data/processors/validate_dates.py
+++ data/processors/validate_dates.py
@@ -14,9 +14,11 @@ for row in reader:
     if row['date_started']:
         date_validated = False
 
+        stripped_date = row['date_started'].strip()
+
         for date_format in ('%m/%d/%y', '%m/%d/%Y', '%Y-%m-%d'):
             try:
-                start_date = datetime.datetime.strptime(row['date_started'], date_format)
+                start_date = datetime.datetime.strptime(stripped_date, date_format)
             except ValueError:  # invalid date
                 continue
             else:
@@ -24,24 +26,17 @@ for row in reader:
                     # sometimes, start dates have the year but not the century,
                     # e.g., 68 instead of 1968. the computer misinterprets years
                     # prior to 1969, such that 68 becomes 2068 instead of 1968,
-                    # etc.
-                    #
-                    # since these are start dates, and people can't have started
-                    # jobs in the future, we know that this is in error. thus,
-                    # we correct these dates by putting them back in the correct
-                    # century, and writing the more precise data to output.
-                    sys.stderr.write('FUTURE DATE: {}\n'.format(row['date_started']))
+                    # etc. since these are start dates, and people can't have
+                    # started jobs in the future, we know that this is in error.
+                    continue
 
-                    precise_date = start_date.replace(year=start_date.year - 100).strftime('%m/%d/%Y')
-
-                    row['date_started'] = precise_date
-
-                date_validated = True
-                break
+                else:
+                    date_validated = True
+                    break
 
         if not date_validated:
             # omit misformatted dates, e.g., 10/1/20007.
-            sys.stderr.write('INVALID DATE: {}\n'.format(row['date_started']))
+            sys.stderr.write('OMITTING INVALID DATE: {}\n'.format(stripped_date))
 
             row['date_started'] = ''
 
