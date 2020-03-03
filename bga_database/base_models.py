29dd925aa51d3d5f73566a17b50173f92a99af7b
Fatima Gomez <fatima.gomez@datamade.us>
2020-03-03 12:16:07 -0600

    validate single preferred name

--- bga_database/base_models.py
+++ bga_database/base_models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 
 
 class SluggedModel(models.Model):
@@ -6,3 +7,25 @@ class SluggedModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class AliasModel(models.Model):
+    name = models.CharField(max_length=255)
+    preferred = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def clean(self):
+        try:
+            preferred_aliases = type(self).objects.filter(
+                Q(preferred=True) & Q(employer_id=self.employer.id)
+            )
+        except len(preferred_aliases) == 0:
+            self.preferred = True
+        except len(preferred_aliases) > 1:
+            other_alias = preferred_aliases.filter(~Q(id=self.id)).first()
+            other_alias.preferred = False
+            other_alias.save()
+
+            self.preferred = True
