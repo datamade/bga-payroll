7488dab4b039d3a461edc4a14c56839e6ef26b19
hancush <hannah.cushman@gmail.com>
2020-03-19 14:49:08 -0500

    Adios, flushing of the queues

--- data_import/tasks.py
+++ data_import/tasks.py
@@ -7,7 +7,7 @@ from celery.signals import task_prerun
 from django.core.management import call_command
 from django.db import connection
 
-from data_import.utils import CsvMeta, ImportUtility, queues
+from data_import.utils import CsvMeta, ImportUtility
 
 
 class DataImportTask(Task):
@@ -125,15 +125,6 @@ def select_unseen_responding_agency(self, *, s_file_id):
 
 
 @shared_task(bind=True, base=DataImportTask)
-def flush_responding_agency_queue(self, *, s_file_id):
-    q = queues.RespondingAgencyQueue(s_file_id)
-
-    q.flush()
-
-    return 'Flushed responding agency queue'
-
-
-@shared_task(bind=True, base=DataImportTask)
 def insert_responding_agency(self, *, s_file_id):
     self.import_utility.insert_responding_agency()
 
@@ -157,15 +148,6 @@ def select_unseen_parent_employer(self, *, s_file_id):
 
 
 @shared_task(bind=True, base=DataImportTask)
-def flush_parent_employer_queue(self, *, s_file_id):
-    q = queues.ParentEmployerQueue(s_file_id)
-
-    q.flush()
-
-    return 'Flushed parent employer queue'
-
-
-@shared_task(bind=True, base=DataImportTask)
 def insert_parent_employer(self, *, s_file_id):
     self.import_utility.insert_parent_employer()
 
@@ -179,15 +161,6 @@ def select_unseen_child_employer(self, *, s_file_id):
     self.update_status('child employer unmatched')
 
     return 'Selected child employers'
-
-
-@shared_task(bind=True, base=DataImportTask)
-def flush_child_employer_queue(self, *, s_file_id):
-    q = queues.ChildEmployerQueue(s_file_id)
-
-    q.flush()
-
-    return 'Flushed child employer queue'
 
 
 @shared_task(bind=True, base=DataImportTask)
