09ddddaa9a614c35d64bdaa1d7a217e3bc485c34
hancush <hannah.cushman@gmail.com>
2020-03-19 09:17:34 -0500

    Remove unused SourceFileHook

--- data_import/views.py
+++ data_import/views.py
@@ -21,44 +21,6 @@ from data_import.tasks import flush_responding_agency_queue, \
 from payroll.models import Employer
 
 
-class SourceFileHook(View):
-    def post(self, request):
-        '''
-        Accept post containing file metadata & ID in Google Drive
-        '''
-        upload = Upload.objects.create()
-        source_files = json.loads(request.POST['source_files'])
-
-        for file_metadata in source_files:
-            agency = file_metadata.pop('responding_agency')
-            responding_agency, _ = RespondingAgency.objects.get_or_create(name=agency)
-
-            file_metadata['upload'] = upload
-            file_metadata['responding_agency'] = responding_agency
-
-            file_metadata = self._hydrate_date_objects(file_metadata)
-
-            SourceFile.objects.create(**file_metadata)
-
-        # TO-DO: Kick off delayed task, which iterates over all source files
-        # without an attached file and calls SourceFile.download_from_drive
-
-        return HttpResponse('Caught!')
-
-    def _hydrate_date_objects(self, file_metadata):
-        '''
-        Convert date strings to Python date objects.
-        '''
-        date_fields = [k for k in file_metadata.keys() if k.endswith('date')]
-
-        for field in date_fields:
-            date_string = file_metadata[field]
-            date_object = datetime.datetime.strptime(date_string, '%Y-%m-%d')
-            file_metadata[field] = date_object
-
-        return file_metadata
-
-
 class StandardizedDataUpload(LoginRequiredMixin, FormView):
     template_name = 'data_import/upload.html'
     form_class = UploadForm
