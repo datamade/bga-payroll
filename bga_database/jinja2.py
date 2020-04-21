a19ca05114f25c4f338f5c0e2945068b79ff7f41
Fatima Gomez <fatima.gomez@datamade.us>
2020-04-21 12:00:33 -0500

    set up static file global var

--- bga_database/jinja2.py
+++ bga_database/jinja2.py
@@ -1,3 +1,4 @@
+from compressor.contrib.jinja2ext import CompressorExtension
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
 from django.contrib import messages
@@ -11,7 +12,7 @@ from payroll.utils import format_ballpark_number, format_salary, \
 
 
 def environment(**options):
-    env = Environment(**options)
+    env = Environment(**options, extensions=[CompressorExtension])
 
     env.globals.update({
         'static': staticfiles_storage.url,
