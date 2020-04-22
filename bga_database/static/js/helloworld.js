20d1a0ab6a368d638760e6b58d62274ae9a57bbc
Fatima Gomez <fatima.gomez@datamade.us>
2020-04-22 10:47:31 -0500

    include babel-plugin-transform-runtime

--- bga_database/static/js/helloworld.js
+++ bga_database/static/js/helloworld.js
@@ -1,6 +1,14 @@
 export function getYearData () {
-	$('#index-data-year').change(function(e) {
-		console.log("Hello, world!");
-
+	$('#index-data-year').change(async function(e) {
+		const url = '/index/?data_year=' + e.currentTarget.value
+		try {
+			const result = await $.ajax({
+				url: url,
+				type: 'GET',
+			})
+			console.log(result)
+		} catch (error) {
+			console.log(error)
+		}
 	})
 }
