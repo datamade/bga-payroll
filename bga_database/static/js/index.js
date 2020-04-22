0667027952419cc5b0996bad60ebe312577676d4
Fatima Gomez <fatima.gomez@datamade.us>
2020-04-22 16:30:09 -0500

    define ajax methods in module where they are called

--- bga_database/static/js/index.js
+++ bga_database/static/js/index.js
@@ -1,3 +1,19 @@
-import { getLatestYearData, getYearData } from "./helloWorld";
+function getYearData () {
+    $('#index-data-year').on('change', async function(e) {
+        const url = '/index/?data_year=' + e.currentTarget.value;
+        try {
+            const result = await $.ajax({
+                url: url,
+                type: 'GET',
+            });
+            ChartHelper.make_salary_chart(result.salary_json, 'employee');
+            $('#index-salary-count').text(result.salary_count);
+            $('#index-unit-count').text(result.unit_count);
+        } catch (error) {
+            console.error(error);
+        }
+        return;
+    });
+}
 
 getYearData();
