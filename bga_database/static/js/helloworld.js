12df00b166f36af6bdce1bdee1e97a07c21492b8
Fatima Gomez <fatima.gomez@datamade.us>
2020-04-22 13:13:42 -0500

    resolve jshint errors

--- bga_database/static/js/helloworld.js
+++ bga_database/static/js/helloworld.js
@@ -1,20 +1,20 @@
 export function getYearData () {
 	$('#index-data-year').change(async function(e) {
-		const url = '/index/?data_year=' + e.currentTarget.value
+		const url = '/index/?data_year=' + e.currentTarget.value;
 		if (e.currentTarget.value !== "Select Year") {
 			try {
 				const result = await $.ajax({
 					url: url,
 					type: 'GET',
-				})
+				});
 				ChartHelper.make_salary_chart(result.salary_json, 'employee');
-				$('#index-salary-count').text(result.salary_count)
-				$('#index-unit-count').text(result.unit_count)
+				$('#index-salary-count').text(result.salary_count);
+				$('#index-unit-count').text(result.unit_count);
 			} catch (error) {
-				console.log("this is an error!")
-				console.log(error)
+				console.log("this is an error!");
+				console.log(error);
 			}
 		}
-		return
-	})
+		return;
+	});
 }
