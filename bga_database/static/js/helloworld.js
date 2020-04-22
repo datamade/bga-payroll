84d3224afb7d772c5f57e0f7eb5942294ca23a9d
Fatima Gomez <fatima.gomez@datamade.us>
2020-04-22 11:10:37 -0500

    render chart based on year

--- bga_database/static/js/helloworld.js
+++ bga_database/static/js/helloworld.js
@@ -1,14 +1,19 @@
 export function getYearData () {
 	$('#index-data-year').change(async function(e) {
 		const url = '/index/?data_year=' + e.currentTarget.value
-		try {
-			const result = await $.ajax({
-				url: url,
-				type: 'GET',
-			})
-			console.log(result)
-		} catch (error) {
-			console.log(error)
+		if (e.currentTarget.value !== "Select Year") {
+			try {
+				const result = await $.ajax({
+					url: url,
+					type: 'GET',
+				})
+				ChartHelper.make_salary_chart(result.salary_json, 'employee')
+				console.log(result.salary_json)
+			} catch (error) {
+				console.log("this is an error!")
+				console.log(error)
+			}
 		}
+		return
 	})
 }
