b2a66ac6b0e5c4a8300a72f3d7e67f9e55ce2ac4
Fatima Gomez <fatima.gomez@datamade.us>
2020-04-23 09:34:58 -0500

    resolve flake8 errors

--- payroll/context_processors.py
+++ payroll/context_processors.py
@@ -7,15 +7,17 @@ from data_import.models import StandardizedFile
 def inspiration_slugs(request):
     try:
         chicago_slug = Unit.objects.get(name__iexact='City of Chicago').slug
-        years = StandardizedFile.objects.distinct('reporting_year')\
-                                                 .values('reporting_year')
-        distinct_years = reversed(years)
     except:
         chicago_slug = None
+
+    years = StandardizedFile.objects\
+        .distinct('reporting_year')\
+        .values('reporting_year')
+    reversed_years = reversed(years)
 
     return {
         'data_year': settings.DATA_YEAR,
         'chicago_slug': chicago_slug,
         'STATIC_URL': settings.STATIC_URL,
-        'distinct_years': distinct_years,
+        'distinct_years': reversed_years,
     }
