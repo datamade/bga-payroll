ee18c9dc6d1442890745a40848164d9fa9e36ae6
Sam McAlilly <sammcalilly@gmail.com>
2021-08-19 10:25:03 -0500

    remove trailing whitespace

--- payroll/views.py
+++ payroll/views.py
@@ -43,7 +43,7 @@ class IndexView(TemplateView, ChartHelperMixin):
         context['show_donate_banner'] = Setting.get('PAYROLL_SHOW_DONATE_BANNER', False)
 
         donate_text = Setting.get('DONATE_MESSAGE', None)
-        context['donate_message'] = bleach.clean(donate_text, 
+        context['donate_message'] = bleach.clean(donate_text,
                                                  tags=['p', 'strong', 'a'],
                                                  attributes={'a': ['href', 'target'], '*': ['class']})
 
