0f882703e87ca30e7302c54590850fe5514e04ae
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2022-10-25 17:09:44 -0400

    get all elements in place

--- bga_database/static/js/story_feed.js
+++ bga_database/static/js/story_feed.js
@@ -3,7 +3,27 @@ function populateStoryFeed() {
         var container = $('#story-feed-stories');
 
         $.each(data.entries, function (idx, entry) {
-            var storyItem = $('<div class="p-3" />');
+            var storyItem = $('<div class="p-3 story-item" />');
+            
+            var itemLeft = $('<div class="story-img col-md-4"/>');
+            var itemRight = $('<div />');
+
+            var image = $('<a />').attr({
+                'href': entry.link,
+                'target': '_blank'
+            });
+            
+            image.append(
+                $('<img />').attr({
+                    'src': '/static/img/newsroom-placeholder.png'
+                })
+            );
+
+            var type = $('<div class="text-uppercase"/>').text(
+                    // TODO: see if there is a category for 
+                    // each story to display
+                    'Investigations'
+                );
 
             var title = $('<h5 />').append(
                 $('<a />').attr({
@@ -15,13 +35,11 @@ function populateStoryFeed() {
             var summary = $('<p class="text-serif my-2" />').text(entry.summary);
             var date = $('<p class="mb-0" />').text(entry.date);
 
-            storyItem.append(title, summary, date);
+            itemLeft.append(image)
+            itemRight.append(type, title, summary, date)
+            storyItem.append(itemLeft, itemRight);
 
             container.append(storyItem);
-
-            if ( idx < data.entries.length - 1 ) {
-                container.append($('<hr class="w-75 mx-auto" />'));
-            }
         });
     });
 }
