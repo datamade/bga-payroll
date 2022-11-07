93514c01b590f42440b569c1d0a2c615a01c07b1
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2022-11-07 17:05:51 -0500

    restore search examples, move curious section

--- bga_database/static/js/story_feed.js
+++ bga_database/static/js/story_feed.js
@@ -1,47 +1,48 @@
 function populateStoryFeed() {
-    $.get('/story-feed/', function (data) {
+    $.get('https://illinois-answers.newspackstaging.com/feed/', function (data) {
         var container = $('#story-feed-stories');
-
-        $.each(data.entries, function (idx, entry) {
-            var storyItem = $('<div class="p-3 story-item" />');
+        print(data);
+        
+        // $.each(data.entries, function (idx, entry) {
+        //     var storyItem = $('<div class="p-3 story-item" />');
             
-            var itemLeft = $('<div class="story-img col-md-4"/>');
-            var itemRight = $('<div />');
+        //     var itemLeft = $('<div class="story-img col-md-4"/>');
+        //     var itemRight = $('<div />');
 
-            var image = $('<a />').attr({
-                'href': entry.link,
-                'target': '_blank'
-            });
+        //     var image = $('<a />').attr({
+        //         'href': entry.link,
+        //         'target': '_blank'
+        //     });
             
-            image.append(
-                // Images from articles not currently available
-                $('<img />').attr({
-                    'src': '/static/img/newsroom-placeholder.png'
-                })
-            );
+        //     image.append(
+        //         // Images from articles not currently available
+        //         $('<img />').attr({
+        //             'src': '/static/img/newsroom-placeholder.png'
+        //         })
+        //     );
 
-            var type = $('<div class="text-uppercase story-detail mb-2"/>').text(
-                    // TODO: grab type of story if possible
-                    'Investigations'
-                );
+        //     var type = $('<div class="text-uppercase story-detail mb-2"/>').text(
+        //             // TODO: grab type of story if possible
+        //             'Investigations'
+        //         );
 
-            // Giving this a class of h3 applies size
-            var title = $('<h5 class="h3 text-black"/>').append(
-                $('<a />').attr({
-                    'href': entry.link,
-                    'target': '_blank'
-                }).text(entry.title)
-            );
+        //     // Giving this a class of h3 applies size
+        //     var title = $('<h5 class="h3 text-black"/>').append(
+        //         $('<a />').attr({
+        //             'href': entry.link,
+        //             'target': '_blank'
+        //         }).text(entry.title)
+        //     );
 
-            var summary = $('<p class="text-serif my-2" />').text(entry.summary);
-            var date = $('<p class="mb-0 story-detail" />').text(entry.date);
+        //     var summary = $('<p class="text-serif my-2" />').text(entry.summary);
+        //     var date = $('<p class="mb-0 story-detail" />').text(entry.date);
 
-            itemLeft.append(image);
-            itemRight.append(type, title, summary, date);
-            storyItem.append(itemLeft, itemRight);
+        //     itemLeft.append(image);
+        //     itemRight.append(type, title, summary, date);
+        //     storyItem.append(itemLeft, itemRight);
 
-            container.append(storyItem);
-        });
+        //     container.append(storyItem);
+        // });
     });
 }
 
